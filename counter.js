324100b3af1f6ee37561b963554b832150ea3809
Felipe Torrejón <felipetorrejon@live.cl>
2018-08-12 17:43:53 -0300

    mejora contador

--- counter.js
+++ counter.js
@@ -34,13 +34,6 @@ function counter(anhoAContar, mesAContar, diaAContar, horaAContar, minutosAConta
   var s = (m - minutos) *60;
   var segundos = parseFloat(s).toFixed(0);
 
-  var quedan = {
-    "dias":dias,
-    "horas":horas,
-    "minutos": minutos,
-    "segundos": segundos
-  };
-
   if (diferencia < 0) {
     return ("¡Ya comenzó!");
   } else if (dias >= 1) {
@@ -49,11 +42,15 @@ function counter(anhoAContar, mesAContar, diaAContar, horaAContar, minutosAConta
     } else {
       return ("Quedan " + dias + " días");
     }
-  } else if (horas >= 1 || dias > 0) {
-    return ("Quedan " + (horas) + " horas y " + minutos + " minutos");
-  } else if (minutos >= 1 || horas > 0) {
-    return ("Quedan " + (minutos + 1) + " minutos"); 
-  } else if (segundos > 0 || minutos > 0) {
+  } else if (horas >= 1) {
+    if (ahora.getMinutes() > minutosAContar){
+      return ("Quedan " + (horas+1) + " horas y " + minutos + " minutos");
+    } else {
+      return ("Quedan " + (horas) + " horas y " + minutos + " minutos")
+    }
+  } else if (minutos >= 1) {
+    return ("Quedan " + (minutos + 1) + " minutos");
+  } else if (segundos >= 0) {
     return ("Quedan " + segundos + " segundos"); 
   }
 
@@ -61,6 +58,6 @@ function counter(anhoAContar, mesAContar, diaAContar, horaAContar, minutosAConta
 
 function iniciar() {
   var elem = document.getElementById("contadorDias");
-  elem.innerHTML = counter(2018, 7, 23, 8, 30, 0);
-  setInterval(function(){elem.innerHTML = counter(2018, 7, 23, 8, 30, 0)}, 1000);
+  elem.innerHTML = counter(2018, 7, 23, 13, 30, 0);
+  setInterval(function(){elem.innerHTML = counter(2018, 7, 23, 13, 30, 0)}, 1000);
 }
